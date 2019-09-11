c538dc3851dc7e5e4f359e8f0e8abdde39338cf3
luke <luke@bitscan.co>
2019-09-11 16:51:34 +1000

    create Layout component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import React, {Component} from 'react'
-import './App.css'
-import injectSheet        from 'react-jss'
-import {connect}          from 'react-redux'
+import React, { Component } from 'react'
 
+import Layout from './components/Layout/Layout';
 
-const Header = injectSheet({
-  yellow: {
-    backgroundColor: 'yellow',
-  },
-})(({classes, name}) => {
-  return (
-    <div className={classes.yellow}>
-      {name}
-    </div>
-  )
-})
-
-const mapStateToProps = (state) => {
-  return {
-    username: state.user.username || ''
-  }
-}
-
-const mapDispatchToProps = (dispatch) => ({
-  updateUsername: (username) => dispatch({type: 'set_username', data: username})
-})
-
-const App = connect(mapStateToProps, mapDispatchToProps)(
   class App extends Component {
     render() {
-      const {username, updateUsername} = this.props
       return (
-        <div className="App">
-          <Header name={username}/>
-          <input placeholder="Username" type="text" onChange={e => updateUsername(e.target.value)}/>
+        <div>
+            <Layout>
+                <p>Test layout</p>
+            </Layout>
         </div>
       )
     }
   }
-)
 
-export default App
+export default App;
