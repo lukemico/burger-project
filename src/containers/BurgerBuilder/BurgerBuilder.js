e9fc922d46a46daa694719b23aca3f43791936c6
luke <luke@bitscan.co>
2019-09-20 14:53:59 +1000

    commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
+class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // };
+    state = {
+           ingredients: {
+               bacon: 1,
+               cheese: 2,
+               salad: 1,
+               meat: 2
+           },
+           totalPrice: 4
+    };
+
+addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+}
+
+removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+        return;
+    }
+
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+}
+
+    render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key <= 0]
+        }
+        return (
+            <Aux>
+                <Burger 
+                    ingredients={this.state.ingredients} />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice} />
+            </Aux>
+        );
+    };
+};
+
+export default BurgerBuilder;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React                 from 'react'
-import ReactDOM              from 'react-dom'
-import {Provider}            from 'react-redux'
-import {createStore}         from 'redux'
-import {rootReducer}         from 'reducers/rootReducers'
-import App                   from './App'
-import registerServiceWorker from 'registerServiceWorker'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer)
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'))
-registerServiceWorker()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
