d66eb485d60471b9549a4048766fb8c3c4a4210c
luke <luke@bitscan.co>
2019-11-04 16:40:30 +1100

    change MOdal + OrderSummary to Class components

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ( props ) => {
-    const ingredientSummary = Object.keys( props.ingredients )
+class OrderSummary extends Component {
+    // This could be a functional component, doesn't have to be a CLass.
+    componentDidUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render () {
+        const ingredientSummary = Object.keys( this.props.ingredients )
         .map( igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li> );
         } );
 
-    return (
-        <Aux>
+        return (<Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-        </Aux>
-    );
-};
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        </Aux>);
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
